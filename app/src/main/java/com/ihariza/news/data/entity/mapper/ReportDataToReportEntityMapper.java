ee9ca7d46189a428b408c37b7a3998d4158e8d45
Ignacio Herrera Ariza <ignacio.herrera@sngular.com>
2020-07-28 09:37:36 +0200

    Refactor

--- app/src/main/java/com/ihariza/news/data/entity/mapper/ReportDataToReportEntityMapper.java
+++ app/src/main/java/com/ihariza/news/data/entity/mapper/ReportDataToReportEntityMapper.java
@@ -13,17 +13,17 @@ public class ReportDataToReportEntityMapper extends Mapper<ReportData, ReportEnt
 
     @Override
     public ReportEntity map(ReportData value) {
-        final ReportEntity report = new ReportEntity();
-        report.setId(value.getId());
-        report.setTitle(value.getTitle());
-        report.setDescription(value.getDescription());
-        report.setUrl(value.getUrl());
-        report.setAuthor(value.getAuthor());
-        report.setImage(value.getImage());
-        report.setLanguage(value.getLanguage());
-        report.setCategory(value.getCategory());
-        report.setPublished(value.getPublished());
-        return report;
+        final ReportEntity reportEntity = new ReportEntity();
+        reportEntity.setId(value.getId());
+        reportEntity.setTitle(value.getTitle());
+        reportEntity.setDescription(value.getDescription());
+        reportEntity.setUrl(value.getUrl());
+        reportEntity.setAuthor(value.getAuthor());
+        reportEntity.setImage(value.getImage());
+        reportEntity.setLanguage(value.getLanguage());
+        reportEntity.setCategory(value.getCategory());
+        reportEntity.setPublished(value.getPublished());
+        return reportEntity;
     }
 
     @Override
--- app/src/main/java/com/ihariza/news/data/entity/mapper/ReportEntityToReportMapper.java
+++ app/src/main/java/com/ihariza/news/data/entity/mapper/ReportEntityToReportMapper.java
@@ -28,16 +28,16 @@ public class ReportEntityToReportMapper extends Mapper<ReportEntity, Report> {
 
     @Override
     public ReportEntity reverseMap(Report value) {
-        final ReportEntity report = new ReportEntity();
-        report.setId(value.getId());
-        report.setTitle(value.getTitle());
-        report.setDescription(value.getDescription());
-        report.setUrl(value.getUrl());
-        report.setAuthor(value.getAuthor());
-        report.setImage(value.getImage());
-        report.setLanguage(value.getLanguage());
-        report.setCategory(value.getCategory());
-        report.setPublished(value.getPublished());
-        return report;
+        final ReportEntity reportEntity = new ReportEntity();
+        reportEntity.setId(value.getId());
+        reportEntity.setTitle(value.getTitle());
+        reportEntity.setDescription(value.getDescription());
+        reportEntity.setUrl(value.getUrl());
+        reportEntity.setAuthor(value.getAuthor());
+        reportEntity.setImage(value.getImage());
+        reportEntity.setLanguage(value.getLanguage());
+        reportEntity.setCategory(value.getCategory());
+        reportEntity.setPublished(value.getPublished());
+        return reportEntity;
     }
 }
