38f8f3f049508fb1f204bef83fb0600276b218bd
Ignacio Herrera Ariza <ignacio.herrera@sngular.com>
2020-07-28 14:11:55 +0200

    Add tests

--- app/src/test/java/com/ihariza/news/presentation/view/util/DateUtilTest.java
+++ app/src/test/java/com/ihariza/news/presentation/view/util/DateUtilTest.java
@@ -1,8 +1,29 @@
 package com.ihariza.news.presentation.view.util;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import static org.junit.Assert.assertTrue;
 
 public class DateUtilTest {
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test(expected = InvocationTargetException.class)
+    public void instantiateClassShouldThrowIllegalStateException()
+            throws NoSuchMethodException, IllegalAccessException,
+            InvocationTargetException, InstantiationException {
+        Constructor<DateUtil> constructor = DateUtil.class.getDeclaredConstructor();
+        assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+        constructor.setAccessible(true);
+        constructor.newInstance();
+    }
 
     @Test
     public void givenyyyyMMddTHHmmss0000FormatShouldReturnsddMMyyyyHHmmhFormat() {
